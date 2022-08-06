9be1bc98dca179ce2cc73a530435087ff20ca572
thetinyspark <contact@the-tiny-spark.com>
2022-08-06 23:40:10 +0200

    add 8 directions animations

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,24 @@
-import {AssetsManager, DisplayObjectContainer, Grid2D, IMAGE_TYPE, INotification, JSON_TYPE, MouseControl, MouseControlEvent, Stage, Stats, Webgl2DRenderer} from "@thetinyspark/moocaccino-barista";
-import Cell from "./Cell";
+import {Animation, AssetsManager, IMAGE_TYPE, INotification, JSON_TYPE, MouseControl, MouseControlEvent, Stage, Stats, Webgl2DRenderer} from "@thetinyspark/moocaccino-barista";
 import GraphicManager from "./GraphicManager";
-import ShapeFactory from "./ShapeFactory";
 // render loop
 
-let map:DisplayObjectContainer = null;
 let stage:Stage = null;
 let stats:Stats = null;
-let grid:Grid2D<Cell> = new Grid2D<Cell>();
-let isDown:boolean = false;
-let filling:boolean = false;
-let config:any = {};
 
 
 function render(){
     stage.nextFrame();
-
-    if( config.ticker < 0 ){
-        window.requestAnimationFrame( 
-            ()=>{
-                render();
-            }
-        );
-    }
-    else{
-        setTimeout( render,config.ticker);
-    }
-}
-
-function keyHandler(event:KeyboardEvent){
-    if( event.type ==="keydown"){
-
-        if( event.key === "a" ){
-            compute();
+    window.requestAnimationFrame( 
+        ()=>{
+            render();
         }
-        if( event.key === "z" ){
-            apply();
-        }
-        if( event.key === "e" ){
-            compute();
-            apply();
-        }
-        if( event.key === "p" ){
-            createCells();
-        }
-        if( event.key === "c" ){
-            cleanCells();
-        }
-        if( event.key === "ArrowDown" ){
-            map.scaleX = map.scaleX - 0.125 < 0.125 ? 0.125 : map.scaleX - 0.125;
-            map.scaleY = map.scaleX;
-        }
-        if( event.key === "ArrowUp" ){
-            map.scaleX = map.scaleX + 0.125 > 1 ? 1 : map.scaleX + 0.125;
-            map.scaleY = map.scaleX;
-        }
-        if( event.key ==="Shift"){
-            filling = true;
-        }
-    }
-    else{
-        filling = false;
-    }
+    );
 }
 
 function preload(){
     window.removeEventListener("load", preload);
     const manager:AssetsManager = new AssetsManager();
-    manager.queue("./assets/map.json", JSON_TYPE, "map");
+    manager.queue("./assets/config.json", JSON_TYPE, "config");
     manager.queue("./assets/sheets/atlas_0.json", JSON_TYPE, "spritesheet_json");
     manager.queue("./assets/sheets/atlas_0.png", IMAGE_TYPE, "spritesheet_img");
     manager.loadQueue().then(
@@ -77,71 +28,40 @@ function preload(){
     );
 }
 
-function compute(){
-    grid.forEach( 
-        (cell:Cell, row:number, col:number)=>{
-            cell.compute(grid);
+function addAnimations(animations:Animation[]){
+    animations.forEach( 
+        (current:Animation, index:number)=>{
+            const row:number = Math.floor(index / 4);
+            const col:number = index % 4;
+            stage.addChild( current );
+            current.width = 128;
+            current.height = 128;
+            current.x = col * 128 + 400;
+            current.y = row * 128 + 400;
+            current.loop = true;
+            current.play();
         }
     );
 }
 
-function apply(){
-    grid.forEach( 
-        (cell:Cell, row:number, col:number)=>{
-            cell.apply();
-        }
-    );
-}
-
-function createMap(map:DisplayObjectContainer){
-    const row:number = config.map.row;
-    const col:number = config.map.col;
-    const size:number = config.map.cellSize;
-    grid.reset(row, col);
-    grid.forEach(
-        (cell:Cell, row:number, col:number)=>{
-            cell = new Cell(size);
-            cell.init(row,col);
-            map.addChild(cell); 
-            grid.addAt(row, col, cell);
-        }
-    );
-}
-
-function cleanCells(){
-    grid.forEach(
-        (cell:Cell, row:number, col:number)=>{
-            cell.living = false;
-        }
-    );
-}
-
-function createCells(){
-    const coords = ShapeFactory.prompt();
-    coords.forEach( 
-        (coord)=>{
-            const cell = grid.getAt(coord.row, coord.col); 
-            if(cell !== null )
-                cell.living = true;
-        }
-    )
-}
-
-function mouseHandler(event:INotification){
-    const type = event.getEventType();
-    const cell = event.getPayload().target;
-
-    if( type === MouseControlEvent.MOUSE_CONTROL_DOWN ){
-        isDown = true;
+function createAnimations(config:any){
+    const animations:Array<Animation> = new Array<Animation>();
+    for( let i:number = 0; i < config.animations.length; i++ ){
+        const name:string = config.animations[i].name;
+        const framestep:number = config.animations[i].framestep;
+        const desc = config.animations[i].frames.map( 
+            (current:any, index:number)=>{
+                return {
+                    texture: GraphicManager.getInstance().getTextureById(current.texture), 
+                    frame: (index * framestep)
+                }
+            }
+        );
+        const anim = Animation.createFromTexturesAndFrames(desc);
+        anim.name = name;
+        animations.push(anim);
     }
-    else if( type === MouseControlEvent.MOUSE_CONTROL_UP ){
-        isDown = false;
-        filling = false;
-    }
-
-    if( isDown){
-        cell.living = filling;
-    }
+    return animations;
 }
 
 function start(manager:AssetsManager){
@@ -149,12 +69,11 @@ function start(manager:AssetsManager){
     // init texture manager
     GraphicManager.getInstance().reset(manager);
 
-    
     // create the scene/stage, note: Stage inherits from DisplayObjectContainer
     stage = new Stage();
     
     // set renderer
-    stage.setRenderer(new Webgl2DRenderer());
+    // stage.setRenderer(new Webgl2DRenderer());
     
     // define stage width and height
     stage.getCanvas().width = window.innerWidth;
@@ -172,29 +91,18 @@ function start(manager:AssetsManager){
     // start monitoring
     stats.start();
     
-    // ad map
-    config = manager.get("map");
-    ShapeFactory.loadPatterns(config.patterns);
     
-    
-    map = new DisplayObjectContainer();
-    createMap(map);
-    // add map
-    stage.addChild(map);
     // add stats object to the stage
     stage.addChild(stats);
     
     // start render loop
     render();
-    const controls = new MouseControl(stage);
-    controls.activate();
 
-    
-    stage.subscribe(MouseControlEvent.MOUSE_CONTROL_MOVE, mouseHandler);
-    stage.subscribe(MouseControlEvent.MOUSE_CONTROL_UP, mouseHandler);
-    stage.subscribe(MouseControlEvent.MOUSE_CONTROL_DOWN, mouseHandler);
+    // ad animations
+    const config:any = manager.get("config");
+    const animations = createAnimations(config);
+    addAnimations(animations);
+
 }
 
 window.addEventListener("load", preload);
-window.addEventListener("keydown", keyHandler, true);
-window.addEventListener("keyup", keyHandler, true);

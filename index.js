678012309c1011c8aa81685460437fe43c5118da
thetinyspark <contact@the-tiny-spark.com>
2022-09-24 18:43:41 +0200

    add pixelate filter

--- index.js
+++ index.js
@@ -1,77 +1,46 @@
-var bolts = [];
-var frame = 0;
-var flashFrame = 0;
-var currentBolt = null;
 
-function createBolts(numBolts, width, height){
-    const bolts = [];
-    for( let i = 0; i < numBolts; i++ ){ 
-        const startX = Math.round( (Math.random() * width/2)  + width/4);
-        const startY = -100;
-
-        const bolt = new Lightning();
-        bolt.start.x = startX;
-        bolt.start.y = startY;
-        bolt.childrenGap = 0.9;
-        bolt.thickness = 4; 
-        bolt.numPoints = Math.round( Math.random() * 10) + 40;
-        bolt.build();
-        bolts.push(bolt);
+class PixelateFilter{
+    constructor(){
+        this._canvas = document.createElement("canvas"); 
+        this._context = this._canvas.getContext("2d");
     }
 
-    return bolts;
+    pixelate(source, destination, factor){
+        const context = this._context; 
+        const destContext = destination.getContext("2d"); 
+
+        this._canvas.width = destination.width;
+        this._canvas.height = destination.height;
+        context.save();
+        context.clearRect(0,0,this._canvas.width, this._canvas.height); 
+        context.scale(1/factor, 1/factor); 
+        context.drawImage(source, 0, 0);
+        context.restore();
+
+        destContext.save();
+        destContext.scale(factor, factor);
+        destContext.drawImage(this._canvas, 0, 0);
+        destContext.restore();
+    }
 }
 
-function drawClouds(context){
-    const img = document.getElementById("cloud"); 
-    context.save();
-    for( let i = 0; i < 20  ; i++ ){
-        context.drawImage(img, 150 * (i-3), -150 + ((i%3) * 75)); 
-    }
-    context.restore();
-}
+const filter = new PixelateFilter();
+var factor = 1;
 
 function tick(){
     const canvas    = document.querySelector("#game");
     const context   = canvas.getContext("2d");
+    const source    = document.getElementById("perso");
     context.clearRect(0,0,canvas.width, canvas.height); 
 
-    if( Math.random() > 0.99 ){
-        flashFrame = Math.round( Math.random() * 80 );
 
-        if( flashFrame > 30 ){
-            const index = Math.round( (bolts.length-1) * Math.random());
-            currentBolt = bolts[index]; 
-            frame = 0;
-        }
-    }
+    const period = Math.abs( Math.sin( factor * Math.PI / 180) * 30 ); 
+    factor++; 
 
-    if( currentBolt !== null ){
-        currentBolt.draw(context, frame * 5); 
-        currentBolt.worldAlpha = (60 - frame) / 60; 
-    }
+    filter.pixelate(source, canvas, period);
 
-    
-    drawClouds(context);
-
-    
-    if( flashFrame > 0 ){
-        context.save();
-        context.globalAlpha = flashFrame / 60; 
-        context.beginPath();
-        context.fillStyle = "white"; 
-        context.fillRect(0,0,canvas.width, canvas.height);
-        context.fill();
-        context.closePath();
-        context.restore();
-        flashFrame--;
-    }
-
-    frame++;
-    setTimeout(tick,16);
+    window.requestAnimationFrame(tick);
 }
-
-
 
 window.addEventListener(
     "load", 
@@ -81,7 +50,7 @@ window.addEventListener(
         const height    = window.innerHeight;
         canvas.width    = width; 
         canvas.height   = height;
-        bolts           = createBolts( 10, width, height);
         tick();
     }
 )
+
